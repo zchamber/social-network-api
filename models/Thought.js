cdd81d3aec6f3262cd0300a1f0ee5ef99a57994d
Monkey D. Luffy <zachchamber07@gmail.com>
2024-02-04 12:46:58 -0700

    beginning models code

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => new Date(timestamp).toISOString()
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Reaction'
+    }
+  ]
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
